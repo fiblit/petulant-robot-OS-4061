cc8a811b3033156e101ba54ff298616b254bb0ab
Dalton <hildr039@umn.edu>
2015-09-29 16:07:57 -0500

    Add main structure comment

--- graphexec.c
+++ graphexec.c
@@ -2,5 +2,20 @@
 
 int main(int argc, char *argv[])
 {
+	/*Main structure
+	fopen("in");
+	while ((line = getLine()) != NULL)
+		getNode(line);
+	//buildGraph(Nodes); //not needed...
+	buildRowList(nodes); //runs "BFS" naturally (it has to loop check to build)
+	for ( int i = 0; i < rowList.size(); i++ ){
+		fan ( rowList[i].size() )
+		if childpid == 0
+			exec (nodeij.prog)
+		else
+			while (r_wait > 0)
+	}
+	free rowlist
+	*/
 	return 0;
 }
