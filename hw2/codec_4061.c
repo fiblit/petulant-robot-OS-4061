f1eadbb8adef2b021e9882f843689081fcd2b272
hildr039 <hildr039@umn.edu>
2015-10-21 00:23:53 -0500

    Clarify a few comments in the design idea(s)
    I also fixed the annoying space before the end of a multi-line comment

--- hw2/codec_4061.c
+++ hw2/codec_4061.c
@@ -8,18 +8,19 @@ int main( int argc, char *argv[] ) {
 	bool isEncode = (argv[1][1] == 'e');
 	
 	/*
-	 //could perhaps be split into two different functions encodeDIR and decodeDIR
+	 mk reportFile
+	 fopen reportfile
+	 //reportFile parameter, so that codeDIR can update it. (Not the name of it, but the actual FILE* is passed)
+	 codeDir(input, output, e/d, reportFile)//Otherwise input and output are just const char *
+	 //codeDir could perhaps be split into two different functions encodeDIR and decodeDIR
 	 //but it is such a trivial difference in the function that I don't see why you would. :P
-	 codeDIR(input, output, e/d)
-	 mkreport??? //(this should go before the call to codeDir. CodeDir then gets a reference to the
-	 //report file, so that it can update it. (Not the name of it, but the actual FILE*)
-	 */
+	*/
 
 	return 0;
 }
 
 /*
-codeDir(input, output, e/d) <- input and output should probably be FILE* (or int fd due to opendir)
+codeDir(input, output, e/d, reportfile) //input and output should be just plain const char * They should NOT be FILE* or fd.
 	DIR *indir = opendir(input)
 	dirent = readDir(indir);
 	while dirent != NULL
@@ -28,5 +29,6 @@ codeDir(input, output, e/d) <- input and output should probably be FILE* (or int
 		else
 			encode/decode file
 			save to output/file
+			write to reportFile
 		dirent = readDir(indir);
- */
+*/
